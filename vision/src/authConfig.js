e293daeea2ef828f1f1624b846c5d0670cf4c964
verma-CBT <verma@cbtarchitects.com>
2025-04-29 14:55:24 -0400

    env file debug

--- vision/src/authConfig.js
+++ vision/src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
   auth: {
     clientId: process.env.REACT_APP_AZURE_CLIENT_ID,
-    authority: `https://login.microsoftonline.com/${REACT_APP_AZURE_TENANT_ID}`,
+    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID}`,
     redirectUri: process.env.REACT_APP_API_BASE_URL,
   },
 };
