99b53470e30e6c636e76aa5bfb20aecc7ff944bc
verma-CBT <verma@cbtarchitects.com>
2025-05-01 15:22:04 -0400

    No more react justt plain vanilla js

--- vision/src/auth/authConfig.js
+++
@@ -1,7 +0,0 @@
-export const msalConfig = {
-  auth: {
-    clientId: process.env.REACT_APP_AZURE_CLIENT_ID,
-    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID}`,
-    redirectUri: process.env.REACT_APP_API_BASE_URL,
-  },
-};
